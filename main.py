c113baea501ed0e48e703034d531d0fade210086
AlfonsoPonce <65920001+AlfonsoPonce@users.noreply.github.com>
2023-10-19 20:03:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -31,12 +31,13 @@ def go(config: DictConfig):
     # Steps to execute
     steps_par = config['main']['steps']
     active_steps = steps_par.split(",") if steps_par != "all" else _steps
-
+    
     # Move to a temporary directory
     with tempfile.TemporaryDirectory() as tmp_dir:
 
         if "download" in active_steps:
             # Download file and load in W&B
+            print("HOLA")
             _ = mlflow.run(
                 os.path.join(hydra.utils.get_original_cwd(), "components", "get_data"),
                 "main",
